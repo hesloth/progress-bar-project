d8b7e3bd504c907ac324eaf84a66c1786874de7f
hesloth <conlanh2004@gmail.com>
2024-01-21 21:57:04 +1100

    Create the JFrame

--- progress.java
+++ progress.java
@@ -1,10 +1,16 @@
 import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class progress {
+public class progress extends Canvas {
 
     public static void main(String[] args) {
-        System.out.println("hi");
-    
+        JFrame frame = new JFrame("Window");
+        Canvas canvas = new progress();
+        canvas.setSize(800, 600);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);    
     }
 }
 
