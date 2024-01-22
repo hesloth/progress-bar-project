185c312d9c2d890093c4e728a133a355744d0691
hesloth <conlanh2004@gmail.com>
2024-01-22 17:07:25 +1100

    canvas drawn with shapes

--- shapes.java
+++ shapes.java
@@ -2,11 +2,13 @@ import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class progress extends Canvas {
+public class shapes extends Canvas {
+
+    int[] toDo = new int[4];
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Window");
-        Canvas canvas = new progress();
+        Canvas canvas = new shapes();
         canvas.setSize(800, 600);
         frame.add(canvas);
         frame.pack();
@@ -14,7 +16,13 @@ public class progress extends Canvas {
     }
 
     public void paint(Graphics g) {
-        g.drawRect(50, 50, 100, 100);
+        g.drawRect(50, 80, 700, 50);
+
+        int j = 140;
+        for (int i = 0; i < toDo.length; i++) {
+            g.drawRect(100, j + 70, 50, 50);
+            j += 70;
+        }
     }
 }
 
