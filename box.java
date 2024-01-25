5549bfd969ce5904cee1871d02decb21d1c72b31
hesloth <conlanh2004@gmail.com>
2024-01-25 19:41:06 +1100

    basic file setup

---
+++ box.java
@@ -0,0 +1,11 @@
+public class box {
+    
+    public box() {
+        int bounding = 50;
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
--- progress.java
+++ progress.java
@@ -1,3 +1,13 @@
-
+public class progress {
+    
+    public progress() {
+        int height = 50;
+        int width = 300;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
 
 
